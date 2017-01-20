2900def6a8787433a86192ba326036dddcd2967f
Edward Gao <egao@sent.com>
2017-01-19 20:46:52 -1000

    queries after indexing

--- indexing.sql
+++ indexing.sql
@@ -12,3 +12,9 @@ CREATE INDEX make_and_model_code_idx ON "car_models" (make_code, model_code);
 
 DROP INDEX IF EXISTS year_idx;
 CREATE INDEX year_idx ON "car_models" (year);
+
+SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM';
+SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM';
+SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015;
+SELECT DISTINCT * FROM "car_models" where year = 2010;
