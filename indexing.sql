c97c8800d885243c94c1afdd4686cb1de574178d
Edward Gao <egao@sent.com>
2017-01-19 20:10:13 -1000

    timing select 3

--- indexing.sql
+++ indexing.sql
@@ -1,3 +1,4 @@
 \timing
 SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM';
 SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM';
