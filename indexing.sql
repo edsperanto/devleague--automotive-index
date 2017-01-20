ee2331844cb35804694900c908ad6bdb588c018c
Edward Gao <egao@sent.com>
2017-01-19 20:21:16 -1000

    record time before indexing

--- indexing.sql
+++ indexing.sql
@@ -1,6 +1,6 @@
 \timing
-SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM';
-SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
-SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM';
-SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015;
-SELECT DISTINCT * FROM "car_models" where year = 2010;
+SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM'; /* 34.211ms */
+SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; /* 55.425ms */
+SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM'; /* 31.627ms */
+SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015; /* 271.374ms */
+SELECT DISTINCT * FROM "car_models" where year = 2010; /* 97.321 */
