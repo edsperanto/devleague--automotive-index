79cdac0a32206935a0142d3facac5d8d05ec3b48
Edward Gao <egao@sent.com>
2017-01-19 20:51:32 -1000

    record time after indexing

--- indexing.sql
+++ indexing.sql
@@ -12,8 +12,8 @@ CREATE INDEX make_code_idx ON "car_models" (make_code);
 CREATE INDEX make_and_model_code_idx ON "car_models" (make_code, model_code);
 CREATE INDEX year_idx ON "car_models" (year);
 
-SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM';
-SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
-SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM';
-SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015;
-SELECT DISTINCT * FROM "car_models" where year = 2010;
+SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM'; /* 1.367ms */
+SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R';  /* 0.535ms */
+SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM'; /* 1.796ms */
+SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015; /* 195.350ms */
+SELECT DISTINCT * FROM "car_models" where year = 2010; /* 33.696ms */
