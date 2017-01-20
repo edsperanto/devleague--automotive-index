3ce3495e17a77c1fb2f5bb795645bb9778d2742b
Edward Gao <egao@sent.com>
2017-01-19 20:48:27 -1000

    move drop index to beginning for controlled test

--- indexing.sql
+++ indexing.sql
@@ -1,16 +1,15 @@
+DROP INDEX IF EXISTS make_code_idx;
+DROP INDEX IF EXISTS make_and_model_code_idx;
+DROP INDEX IF EXISTS year_idx;
+
 SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM'; /* 34.211ms */
 SELECT DISTINCT model_title FROM "car_models" WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; /* 55.425ms */
 SELECT make_code, model_code, model_title, year FROM "car_models" WHERE make_code = 'LAM'; /* 31.627ms */
 SELECT DISTINCT * FROM "car_models" where year >= 2010 AND year <= 2015; /* 271.374ms */
 SELECT DISTINCT * FROM "car_models" where year = 2010; /* 97.321 */
 
-DROP INDEX IF EXISTS make_code_idx;
 CREATE INDEX make_code_idx ON "car_models" (make_code);
-
-DROP INDEX IF EXISTS make_and_model_code_idx;
 CREATE INDEX make_and_model_code_idx ON "car_models" (make_code, model_code);
-
-DROP INDEX IF EXISTS year_idx;
 CREATE INDEX year_idx ON "car_models" (year);
 
 SELECT DISTINCT make_title FROM "car_models" WHERE make_code = 'LAM';
